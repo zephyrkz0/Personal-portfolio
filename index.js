abb9e41f5837cc54a8ae300ed282aa2b34561a20
zephyrkz0 <needtochangelibur@gmail.com>
2025-06-30 14:47:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_gwpy8x8";
+  const templateID = "template_feck84z";
+
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your message sent successfully!!")
+
+    })
+    .catch(err=>console.log(err));
+
+}
